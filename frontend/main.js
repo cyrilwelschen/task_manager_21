42bfaa777fcb0731979edc0f266ccb9115dd02f1
Cyril Welschen <cj.welschen@gmail.com>
2021-02-10 09:21:27 +0100

    frontend prio sorting started

--- frontend/main.js
+++ frontend/main.js
@@ -13,21 +13,32 @@ function constructSoftPrioCatCols(categories) {
 }
 
 function addSoftPrioTodos(todos) {
-    todos.forEach(todo => {
+    const sortedTodos = sortTodosFromPrio(todos, "soft_prio");
+    sortedTodos.forEach(todo => {
         const todoDiv = document.createElement('div');
         todoDiv.className = 'todo';
         todoDiv.innerHTML = todo.description;
+        todoDiv.title = JSON.stringify(todo, null, 2);
         todoDiv.setAttribute('id', `todo-${getTodoIdFromUrl(todo.url)}`);
         const catDiv = document.getElementById(todo.category);
         catDiv.appendChild(todoDiv);
     })
 }
 
+function sortTodosFromPrio(todos, prio) {
+    if (prio == "hard_prio") {
+        return todos.sort((a, b) => (a.hard_prio > b.hard_prio) ? -1 : 1);
+    }
+    return todos.sort((a, b) => (a.soft_prio > b.soft_prio) ? -1 : 1);
+}
+
 function addHardPrioTodos(todos) {
-    todos.forEach(todo => {
+    const sortedTodos = sortTodosFromPrio(todos, "hard_prio");
+    sortedTodos.forEach(todo => {
         const todoDiv = document.createElement('div');
         todoDiv.className = 'todo hp-todo';
         todoDiv.innerHTML = todo.description;
+        todoDiv.title = JSON.stringify(todo, null, 2);
         todoDiv.setAttribute('id', `todo-${getTodoIdFromUrl(todo.url)}`);
         const catDiv = document.getElementById('hard-prio-grid');
         catDiv.appendChild(todoDiv);
