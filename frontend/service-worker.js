ff53073bb43704c64779d0df9ddfd4c9bd7f0258
Cyril Welschen <cj.welschen@gmail.com>
2021-02-23 07:45:17 +0100

    sw update with new cache version to update static

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,9 +1,20 @@
+const filesToCache = [
+    '/',
+    './modal.css',
+    './style.css',
+    './images/uhr.png',
+];
+
+const staticCacheName = 'pages-cache-v1';
+
 self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
     event.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./modal.css", "./style.css", "./images/uhr.png"])
+        caches.open(staticCacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
         })
-    )
+    );
 });
 
 self.addEventListener('fetch', event => {
